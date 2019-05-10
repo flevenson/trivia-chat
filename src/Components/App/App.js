bad6e0d39f277b7bc60e1bdc56f42f7259ebcb48
Freddie Levenson <freddielev@gmail.com>
2019-05-10 14:35:29 -0600

    Add test user and asset folder with test image

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { MessageList, MessageInput } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+
+import 'stream-chat-react/dist/css/index.css';
+
+const client = new StreamChat('hvup4mvjsmc2');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiamxhaGV5In0.yGezD5VjSrYHM9foL3qrH8GwYx9Alq96oENpXJkMVgw'
+
+chatClient.setUser(
+  {
+    id:'fLevenson',
+    name:'Freddie Levenson',
+    image: '../../assets/freddiePic.png'
+  },
+  userToken
+)
 
 function App() {
   return (
