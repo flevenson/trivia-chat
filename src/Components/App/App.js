636826c77b159521dd5c412adf3ac2853149c849
Freddie Levenson <freddielev@gmail.com>
2019-05-10 15:49:44 -0600

    Hide API key

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
 import { MessageList, MessageInput } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
+import  Info  from '../../assets/info.js'
 
 import 'stream-chat-react/dist/css/index.css';
 
-const chatClient = new StreamChat('hvup4mvjsmc2');
-const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiamxhaGV5In0.yGezD5VjSrYHM9foL3qrH8GwYx9Alq96oENpXJkMVgw'
+const chatClient = new StreamChat(Info.apiKey);
+const userToken = Info.sampleUserToken
 
 chatClient.setUser(
   {
